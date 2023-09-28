210e8ba5527ec698583e3dd9bf12d4c4f343446f
mozman <me@mozman.at>
2023-09-28 10:15:21 +0200

    refactor UI

--- scripts/my-size-presets.py
+++ scripts/my-size-presets.py
@@ -8,44 +8,33 @@ from modules.ui_components import FormRow
 
 TITLE = "My Image Size Presets"
 MIN_WIDTH = 120
-HEADER =  [
-    "Presets",
-    "Portrait",
-    "Portrait",
-    "Square",
-    "Landscape",
-    "Landscape",
-    "Widescreen",
-]
 
-SD15_512_PRESETS = "SD15 - 512px", [
-    "512x768 (2:3)",
-    "512x672 (3:4)",
+PRESET_512 = "512", [
     "512x512 (1:1)",
     "672x512 (4:3)",
     "768x512 (3:2)",
     "896x512 (16:9)",
 ]
-SD15_640_PRESETS = "SD15 - 640px", [
-    "640x960 (2:3)",
-    "640x848 (3:4)",
+PRESET_640 = "640", [
     "640x640 (1:1)",
     "848x640 (4:3)",
     "960x640 (3:2)",
     "1136x640 (16:9)",
 ]
-SDXL_PRESETS = "SDXL", [
-    "832x1216 (2:3)",
-    "896x1152 (3:4)",
+PRESET_768 = "768", [
+    "768x768 (1:1)",
+    "1024x768 (4:3)",
+    "1152x768 (3:2)",
+    "1360x768 (16:9)",
+]
+PRESET_SDXL = "SDXL", [
     "1024x1024 (1:1)",
     "1152x896 (4:3)",
     "1216x832 (3:2)",
     "1344x768 (16:9)",
 ]
 
-PRESETS = [
-    SD15_512_PRESETS, SD15_640_PRESETS, SDXL_PRESETS
-]
+PRESETS = [PRESET_512, PRESET_640, PRESET_768, PRESET_SDXL]
 DEFAULT_WIDTH_HEIGHT = 512, 512
 
 
@@ -63,17 +52,9 @@ class MyImageSizePresets(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img: bool):
-        if is_img2img:
-            wc, hc = self.i2i_w, self.i2i_h
-        else:
-            wc, hc = self.t2i_w, self.t2i_h
-
-        outputs = [wc, hc]
+        outputs = [self.i2i_w, self.i2i_h] if is_img2img else [self.t2i_w, self.t2i_h]
         with gr.Group():
             with gr.Accordion(TITLE, open=False):
-                with FormRow():
-                    for value in HEADER:
-                        gr.Button(value=value, interactive=False, min_width=MIN_WIDTH)
                 for label, presets in PRESETS:
                     with FormRow():
                         gr.Button(value=label, min_width=MIN_WIDTH, interactive=False)
@@ -86,7 +67,6 @@ class MyImageSizePresets(scripts.Script):
 
     def after_component(self, component: gr.components.Component, **kwargs):
         element_id = kwargs.get("elem_id")
-
         if isinstance(component, gr.components.Slider):
             if element_id == "txt2img_width":
                 self.t2i_w: gr.components.Slider = component
